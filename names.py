3558af875ccf7b88914c81e6f7237118e47f1f5a
Anatoliy <anbrikzone@gmail.com>
2024-04-25 17:25:47 +0500

    Update names.py

--- names.py
+++ names.py
@@ -1,8 +1,10 @@
 from os import path
 
+# Формируем пути до файлов со списком имен 
 path_names_man = path.join(path.dirname(__file__), 'names', 'male_names_rus.txt')
 path_names_woman = path.join(path.dirname(__file__), 'names', 'female_names_rus.txt')
 
+# Получаем список мужских имен
 def get_names_man() -> list:
     names = []
     path = path_names_man
@@ -11,7 +13,8 @@ def get_names_man() -> list:
             names.append({'name': line.strip()})
 
     return names
-
+    
+# Получеам список женских имен
 def get_names_woman() -> list:
     names = []
     path = path_names_woman
